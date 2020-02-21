51ff00f17273d1fba077678c61d5182e8f916fd0
Nicholas Jones (RIT Student) <nxj8268@ad.rit.edu>
2020-02-21 16:52:39 -0500

    some enemy stuff

---
+++ frougeLikeGame/Assets/Scripts/Turtle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turtle : MonoBehaviour
+{
+    [SerializeField]
+    private int health, damage;
+    private GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(target.transform.position);
+    }
+}

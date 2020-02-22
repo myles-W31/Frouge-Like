f53d79c0ce859ba8ff40940c7e1c4bc2147ebd06
Samuel Carr <Samuel Carr@LAPTOP-GVPPVHU2>
2020-02-22 00:38:34 -0500

    Player Control
    Added the ability to control the player and submerge under the water

---
+++ frougeLikeGame/Assets/Scripts/InputManager.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject mainCamera;
+
+    private Plane plane;
+    private float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        plane = new Plane(Vector3.up, Vector3.zero);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        SurfaceState();
+
+        GetAngleToMouse();
+    }
+
+    public void Move()
+    {
+        // Add acceleration to velocity
+        player.GetComponent<Player>().acceleration =
+            player.GetComponent<Player>().rateOfAccel *
+            player.GetComponent<Player>().direction;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.GetComponent<Player>().direction = Vector3.forward;
+            IncreaseVelocity();
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            player.GetComponent<Player>().direction = -Vector3.right;
+            IncreaseVelocity();
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            player.GetComponent<Player>().direction = -Vector3.forward;
+            IncreaseVelocity();
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            player.GetComponent<Player>().direction = Vector3.right;
+            IncreaseVelocity();
+        }
+        else
+        {
+            player.GetComponent<Player>().velocity *=
+                player.GetComponent<Player>().deaccelerationMultiplier;
+        }
+
+        // Move Player along velocity
+        //velocity = speed * direction;
+
+        // Limit vel vector with ClampMagnitude
+        player.GetComponent<Player>().velocity =
+        Vector3.ClampMagnitude(player.GetComponent<Player>().velocity,
+        player.GetComponent<Player>().maxSpeed);
+
+        // Add vel to position
+        player.GetComponent<Player>().playerPosition +=
+            player.GetComponent<Player>().velocity;
+    }
+
+    public void SurfaceState()
+    {
+        switch (player.GetComponent<Player>().submergeState)
+        {
+            case SubmergeState.Surfaced:
+                if (Input.GetKey(KeyCode.Space))
+                    player.GetComponent<Player>().submergeState = SubmergeState.Submerging;
+                break;
+
+            case SubmergeState.Submerging:
+                if(player.GetComponent<Player>().playerPosition.y > player.GetComponent<Player>().submergeDepth)
+                {
+                    player.GetComponent<Player>().playerPosition.y -= player.GetComponent<Player>().submergeSpeed;
+                }
+                else
+                {
+                    player.GetComponent<Player>().submergeState = SubmergeState.Submerged;
+                }
+                break;
+
+            case SubmergeState.Submerged:
+                if (Input.GetKey(KeyCode.Space))
+                    player.GetComponent<Player>().submergeState = SubmergeState.Surfacing;
+                break;
+
+            case SubmergeState.Surfacing:
+                if (player.GetComponent<Player>().playerPosition.y < player.GetComponent<Player>().surfaceHeight)
+                {
+                    player.GetComponent<Player>().playerPosition.y += player.GetComponent<Player>().submergeSpeed;
+                }
+                else
+                {
+                    player.GetComponent<Player>().submergeState = SubmergeState.Surfaced;
+                }
+                break;
+        }
+    }
+
+    public void GetAngleToMouse()
+    {
+        // Raycast the mouse positon and find the angle of rotation from the player
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (plane.Raycast(ray, out distance))
+        {
+            Vector3 target = ray.GetPoint(distance);
+            Vector3 direction = target - player.GetComponent<Player>().playerPosition;
+            player.GetComponent<Player>().angleOfRotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            // player.GetComponent<Player>().transform.rotation = Quaternion.Euler(0, rotation, 0);
+        }
+    }
+
+    // Helper function to increase the player's velocity by adding acceleration
+    public void IncreaseVelocity()
+    {
+        player.GetComponent<Player>().velocity +=
+            player.GetComponent<Player>().acceleration;
+    }
+}
---
+++ frougeLikeGame/Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SubmergeState
+{
+    Surfaced,
+    Surfacing,
+    Submerged,
+    Submerging
+}
+
+public class Player : MonoBehaviour
+{
+    // Alter these to modify movement properties
+    public float rateOfAccel;
+    public float maxSpeed;
+    public float rotationSpeed;
+    public float angleOfRotation;
+    public float deaccelerationMultiplier;
+
+    // DO NOT TOUCH - altered via Update()
+    public Vector3 playerPosition = new Vector3(0.0f, 0.0f, 0.0f);
+    public Vector3 direction = new Vector3(1.0f, 0.0f, 0.0f);
+    public Vector3 velocity = new Vector3(0.0f, 0.0f, 0.0f);
+    public Vector3 acceleration = new Vector3(0.0f, 0.0f, 0.0f);
+
+    // Submerge SM
+    public SubmergeState submergeState;
+    // Height while on the surface
+    public float surfaceHeight;
+    // Depth when using dive ability
+    public float submergeDepth;
+    // Speed at which the player dives and surfaces
+    public float submergeSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        submergeState = SubmergeState.Surfaced;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetTransform();
+    }
+
+    public void SetTransform()
+    {
+        // Rotate the vehicle to face toawared the mouse pointer
+        transform.rotation = Quaternion.Euler(0, angleOfRotation, 0);
+
+        // Debug.Log(angleOfRotation);
+        // Position vehicle
+        transform.position = playerPosition;
+    }
+}
